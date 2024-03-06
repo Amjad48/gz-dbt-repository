66b1baf4fafd8197ae3c7cf033802e80b420e784
Aaamjad1994@gmail.com <Aaamjad1994@gmail.com>
2024-03-06 11:17:09 +0000

    d

--- models/intermediate/int_orders_operational.sql
+++ models/intermediate/int_orders_operational.sql
@@ -1,7 +1,5 @@
 select
 *,
-orders_id,
-date_date,
 margin + shipping_fee - logcost - cast(ship_cost as float64) AS Operational_margin
 from {{ref("int_orders_margin")}}
 left join {{ref("stg_raw__ship")}}
